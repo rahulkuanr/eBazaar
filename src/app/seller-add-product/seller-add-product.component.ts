6c228bf3c518756196c49875c8512f5cb9a33b28
rahulkuanr <rahulkuanr@gmail.com>
2023-08-04 22:56:21 +0530

    add product API integration

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data-type';
 
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
-  styleUrls: ['./seller-add-product.component.css']
+  styleUrls: ['./seller-add-product.component.css'],
 })
 export class SellerAddProductComponent implements OnInit {
+  productAddedMessage: string | undefined;
+  constructor(private productService: ProductService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  addNewProduct(data: Product) {
+    this.productService.addProduct(data).subscribe(result => {
+      console.log(result);
+      if(result) {
+        this.productAddedMessage = "Product Added Successfully";
+      }
+      setTimeout(() => {this.productAddedMessage = undefined}, 3000);
+    });
   }
-
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../data-type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  addProduct(data: Product) {
+    return this.httpClient.post('http://localhost:3000/products', data);
+  }
+}
