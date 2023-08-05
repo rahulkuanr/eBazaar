23f14b767d07153d6bf22412d6963798809c3f2f
rahulkuanr <rahulkuanr@gmail.com>
2023-08-05 20:06:09 +0530

    update product

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,18 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../data-type';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-seller-update-product',
   templateUrl: './seller-update-product.component.html',
-  styleUrls: ['./seller-update-product.component.css']
+  styleUrls: ['./seller-update-product.component.css'],
 })
 export class SellerUpdateProductComponent implements OnInit {
   productUpdatedMessage: undefined | string;
-  constructor() { }
+  productData: undefined | Product;
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id');
+    productId &&
+      this.productService.getProduct(productId).subscribe((data) => {
+        this.productData = data;
+      });
   }
 
-  addNewProduct(data: Product) {}
-
+  updateProduct(data: Product) {
+    if(this.productData) {
+      data.id = this.productData.id;
+    }
+    this.productService.updateProduct(data).subscribe((result) => {
+      if (result) {
+        this.productUpdatedMessage = 'Product Updated Successfully';
+      }
+      setTimeout(() => {
+        this.productUpdatedMessage = undefined;
+        this.router.navigate(['seller-home']);
+      }, 3000);
+    });
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,21 +3,30 @@ import { Injectable } from '@angular/core';
 import { Product } from '../data-type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   addProduct(data: Product) {
     return this.httpClient.post('http://localhost:3000/products', data);
   }
 
   getProductList() {
-    return this.httpClient.get<Array<Product>>('http://localhost:3000/products');
+    return this.httpClient.get<Array<Product>>(
+      'http://localhost:3000/products'
+    );
   }
 
-  deleteProduct(id:number) {
-    return this.httpClient.delete(`http://localhost:3000/products/${id}`)
+  deleteProduct(id: number) {
+    return this.httpClient.delete(`http://localhost:3000/products/${id}`);
+  }
+
+  getProduct(id: string) {
+    return this.httpClient.get<Product>(`http://localhost:3000/products/${id}`);
+  }
+
+  updateProduct(product: Product) {
+    return this.httpClient.put(`http://localhost:3000/products/${product.id}`, product);
   }
 }
